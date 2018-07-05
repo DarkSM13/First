62389633d2f1dd64cdd2ad44ea707f400af2aa6b
Developer12 <Developer12@OPTIMUS1>
2018-07-05 08:09:45 +0200

    Add a new class

--- First/src/App.java
+++ First/src/App.java
@@ -1,8 +1,33 @@
+class Person{
+	private String Name;
+	private String Surname;
+	
+	private int age;
+	
+	public void setName(String Name) {
+		this.Name=Name;
+	}
+	public String getName() {return this.Name;}
+	
+	public void setSurname(String Surnaame) {
+		this.Surname=Surname;
+	}
+	public String getSurname() {return this.Surname;}	
+	
+	Person(String Name,String Surname,int age) {this.Name=Name;this.Surname=Surname;this.age=age;}
+	
+}
+
+
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Person ja = new Person("Szymon","Margañski",21);
+		
+		System.out.println(ja.getName());
+		
 
 	}
 
