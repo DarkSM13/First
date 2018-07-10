0b5c49e3076ccd05aa65f5a5ae9a985643c71d50
Developer12 <Developer12@OPTIMUS1>
2018-07-10 10:12:47 +0200

    50

--- First/src/SM/ReadObject.java
+++ First/src/SM/ReadObject.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class ReadObject {
 
@@ -13,10 +14,24 @@ public class ReadObject {
 			
 			ObjectInputStream Os = new ObjectInputStream(fi);
 			
-			Person person1=(Person)Os.readObject();
-			Person person2=(Person)Os.readObject();
-			System.out.println(person1);
-			System.out.println(person2);
+			//Person person1=(Person)Os.readObject();
+			ArrayList<Person> List1 = new ArrayList<Person>();
+			
+			List1=(ArrayList<Person>) Os.readObject();
+		//	Person person2=Os.readObject();
+			
+			for(Person x:List1)
+				System.out.println(x);
+			Person mike = new Person(543,"Mike");
+			int d=-1;
+			for(int l=0;l<List1.size();l++)
+			{
+			
+			  if(mike.equals(List1.get(l))) d=l;
+			}
+			System.out.println(d);
+			
+			System.out.println();
 			Os.close();
 			
 		} catch (FileNotFoundException e) {
--- First/src/SM/WriteObject.java
+++ First/src/SM/WriteObject.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class WriteObject {
 
@@ -13,13 +14,20 @@ public class WriteObject {
 		
 		Person mike = new Person(543,"Mike");
 		Person sue = new Person(123,"Sue");
+		Person adam = new Person(533,"Adam");
+		
+		ArrayList<Person> MyList = new ArrayList<Person>();
+		
+		MyList.add(mike);MyList.add(sue);MyList.add(adam);
+		
+		
 		
 		try(FileOutputStream fs = new FileOutputStream("people.bin")){
 			
 		ObjectOutputStream os = new ObjectOutputStream(fs);	
 			
-		os.writeObject(mike);
-		os.writeObject(sue);
+
+		os.writeObject(MyList);
 		
 		os.close();
 		} catch (FileNotFoundException e) {
