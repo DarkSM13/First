9e29194873e4e35a22e0290a9200a79a1eda2431
Developer12 <Developer12@OPTIMUS1>
2018-07-10 08:28:35 +0200

    43

--- First/src/SM/App.java
+++ First/src/SM/App.java
@@ -8,47 +8,26 @@ import java.io.IOException;
 
 public class App {
 
-	
-	public static void main(String[] args) {
-	
-	
+	public static void main (String[] args) {
+		
 		File file = new File("text.txt");
-		BufferedReader br=null;
-
-		try {
-			FileReader fr = new FileReader(file);
-			br = new BufferedReader(fr);
+		
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(file))){
 			
 			String line;
 			
-			while((line = br.readLine())!=null)
-				System.out.println(line);
-			
+			while((line=br.readLine())!=null)
+					System.out.println(line);
 			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("File not found:"+file.toString());
+			System.out.println("Can't find file!");
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Unable to read file:"+file.toString());
-		}
-	
-		finally {
-			
-			try {
-				br.close();
-			}
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Unable to close file:"+file.toString());
-			}
-			catch (NullPointerException ex) {
-				// TODO Auto-generated catch block
-				System.out.println("Null pointer of file:"+file.toString());
-			}
-			
+
+			System.out.println("Unable to read file!");
 		}
 		
-	
+		
 	}
 }
